b823eed2aa4817463bffcaed0c4a9c83aad960f5
mark <mhurwitzus@gmail.com>
2012-12-15 17:50:40 -0600

    building out collection and collection view

--- static/app/app.js
+++ static/app/app.js
@@ -18,22 +18,21 @@ window.App = {
 };
 
 
-// Helper methods
-// for templates
-//---------------
+// Helper methods for templates
+//-----------------------------
 window.template = function(id) {
 	return _.template( $('#' + id).html() );
 };
 
 
 // Incident-- single
-//---------------
+//------------------
 App.Models.Incident = Backbone.Model.extend({
 });
 
 
 // Incident View -- single
-//---------------------
+//------------------------
 App.Views.Incident = Backbone.View.extend({
 	tagName: 'div',
 
@@ -50,9 +49,34 @@ App.Views.Incident = Backbone.View.extend({
 });
 
 
+// Incidents Collection -- many
+// ----------------------------
+App.Collections.Incidents = Backbone.Collection.extend({
+	model: App.Models.Incident
+});
+
+
+// Incidents Collection View -- many
+// ---------------------------------
+App.Views.Incidents = Backbone.View.extend({
+	tagName: 'div',
+
+	render: function(){
+		//filter all incidents
+		//for each create new view
+		//render view and append the el to the collection el
+	}
+});
+
+
+
+//  Bootstrap and run the app
+// --------------------------
+
 console.log('app.js is now running on this page!');
 
 
+// sample data for single incident
 var incident = new App.Models.Incident({
 	description: 'I forgot my lunch and yelled fartface loudly.',
 	feelings:[
@@ -69,7 +93,16 @@ var incident = new App.Models.Incident({
 
 });
 
+//new single incident view
 var incidentView = new App.Views.Incident({ model: incident });
+
+//setup a new collection to contain incidents
+var incidents = new App.Collections.Incidents();
+incidents.add(incident);
+
+//create new collection view instance
+var incidentsView = new App.Views.Incidents({
+});
 
 $('#app').append(incidentView.render().el);
 
